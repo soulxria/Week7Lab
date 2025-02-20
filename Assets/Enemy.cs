3b7da852446129eb21d4d38ec2f99b006fd26cb4
Dylan/Nyx <163457812+soulxria@users.noreply.github.com>
2025-02-20 13:21:58 -0500

    Enemy/Item Scripts and Walls :3

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private float MoveSpeed = 100f;
+    private Rigidbody _rb;
+    public float timer = 2.0f;
+    private bool MovingLeft = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer > 0)
+        {
+            if(!MovingLeft)
+                _rb.MovePosition(this.transform.position + this.transform.right * MoveSpeed * Time.deltaTime);
+            else
+                _rb.MovePosition(this.transform.position - this.transform.right * MoveSpeed * Time.deltaTime);
+
+        }
+        if (timer <= 0)
+        {
+            timer = 2.0f;
+            if (!MovingLeft)
+                MovingLeft = true;
+            else
+                MovingLeft = false;
+        }
+
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player detected - attack!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("Player out of range, resume patrol");
+        }
+    }
+
 }
--- Assets/Item.cs
+++ Assets/Item.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.name == "Player")
+        {
+            Destroy(this.transform.gameObject);
+            Debug.Log("Item collected!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
